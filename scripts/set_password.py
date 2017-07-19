497cff6e4887eeffac41246874b1bfd1880ea2a1
Eric <ericovis@gmail.com>
2017-07-19 11:29:10 -0300

    initial commit

---
+++ scripts/set_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+ENCODING='utf-8'
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("admin_password", help="Admin password for the Scalr server")
+args = parser.parse_args()
+
+input_file = '/etc/scalr-server/scalr-server-secrets.json'
+
+
+with open(inputFile) as f:
+    output = json.load(f)
+
+output['app']['admin_password'] = admin_password
+
+with open(input_file, 'w') as f:
+    json.dump(output, f)
